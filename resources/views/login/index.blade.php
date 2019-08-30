debe485d60884d0da85dbb3d3f852f014b939844
cooldeark <45022034+cooldeark@users.noreply.github.com>
2019-08-30 17:09:45 +0800

    fix

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,7 +8,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"><!--css about font-awesome-->
-<link rel="stylesheet" href="css/product.css">
+<link rel="stylesheet" href="css/index.css">
 <title>James's Category</title>
     </head>
 <body style="">
@@ -35,7 +35,9 @@
         </div>
     </div>
         </section>
-        
+    <div id="mainContainer">
+
+    </div>    
 <script>
 //Based on the Scroller function from @sallar
 var showContent = $('header .content'),showBlur=$('header .overlay'),wHeight= $(window).height();
@@ -98,7 +100,6 @@ Scroller.prototype = {
     });
   }
 };
-
 
 var scroller = new Scroller();  
 scroller.init();

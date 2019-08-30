a807ec74253c024247355071bf4e14d754bed82f
cooldeark <45022034+cooldeark@users.noreply.github.com>
2019-08-30 17:47:12 +0800

    fix all

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,38 +1,27 @@
 <!doctype html>
 <html>
-    <head>
-
+<head>
+<link rel="stylesheet" href="css/index.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<<<<<<< HEAD
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"><!--css about font-awesome-->
-<link rel="stylesheet" href="css/index.css">
-=======
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"><!--css about font-awesome-->
-<link rel="stylesheet" href="css/flexslider.css" type="text/css">
-<<<<<<< HEAD
->>>>>>> parent of c9a1a10... updateIndex
-=======
->>>>>>> parent of c9a1a10... updateIndex
+
 <title>James's Category</title>
-    </head>
-    <body style="background-color:#4D4D4F;overflow-x: hidden;">
-        <div id="logOutButton" style="margin-top:1%;margin-right:1%;cursor:pointer;" class="text-right" ><a class="text-white " href="{{ url('/logout') }}"><b>登出</b></a></div>
-        <div id=" blog-header headTopContainer" class="container">
-            <div class=" py-3 row align-items-center">
-                <div class="col-12">
-                        <h1 class=" text-center blog-header-logo text-white">Presentation</h1>
-                        
-                </div>
-                
-            </div>
-            <!-- <ul class="nav nav-list"><li class="divider"></li></ul> -->
-            
+</head>
+<body style="">
+    <header>
+        <div class="content">
+            <hgroup>
+            <h1>Hi ! I'm James.</h1>
+            <i>Know Me More</i>
+            </hgroup>
         </div>
-        <hr size="10" color="white" style="margin-left:15%;margin-right:15%;"><!--this is line to separate -->
+        <div class="overlay"></div>
+    </header>
+    <section class="site">
         <div id="headerContainer" class="container"><!--container is 置中-->
             <div class="nav-scroller py-1 mb-2">
                 <nav  class="nav justify-content-between">
@@ -40,102 +29,101 @@
                     <a href="{{url('/aboutMe')}}" id="aboutMe" class="p-2 text-white"><i class="fa fa-address-card"></i> 關於我</a>
                     <a href="{{url('/index')}}" class="p-2 text-white"><i class="fa fa-list-alt"></i> 作品資訊</a>
                     <a href="{{url('/index')}}" class="p-2 text-white"><i class="fa fa-child"></i> 聯絡我</a>
-                 
+                    <a href="{{url('/logout')}}" class="p-2 text-white"><i class="fa fa-blind"></i> 登出</a>
                 </nav>
             </div>
         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    </div>
-        </section>
+    </section>
+        <button id="test">test</button>
+        
+
+    
+        
     <div id="mainContainer">
 
     </div>    
-=======
-        <div style="" id="mainContainer">
-                <div style="margin-top:10%" class="">
-                        <h1 class="text-white text-center" style="font-size:50px;">Hi ! Please get more information from here to know me . </h1>
-                    </div>
-        </div>
-        <!--以下是list的畫面，應該放在商品資訊那頁-->
-        <!-- <div id="categoryAndImage" class="container " >
-            <div id="watchsCategory" class="col-3" style="margin-left:-12%;">
-                <ul class="list-group" style="">
-                        <li class="list-group-item list-group-item-dark ">Cras justo odio</li>
-                        <li class="list-group-item list-group-item-dark">Dapibus ac facilisis in</li>
-                        <li class="list-group-item list-group-item-dark">Morbi leo risus</li>
-                        <li class="list-group-item list-group-item-dark">Porta ac consectetur ac</li>
-                        <li class="list-group-item list-group-item-dark">Vestibulum at eros</li>
-                </ul>
-            </div>
-        </div> -->
-
->>>>>>> parent of c9a1a10... updateIndex
 <script>
+//Based on the Scroller function from @sallar
+var $content = $('header .content')
+  , $blur    = $('header .overlay')
+  , wHeight  = $(window).height();
 
+$(window).on('resize', function(){
+  wHeight = $(window).height();
+});
+
+window.requestAnimFrame = (function()
+{
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+function Scroller()
+{
+  this.latestKnownScrollY = 0;
+  this.ticking            = false;
+}
+
+Scroller.prototype = {
+ 
+  init: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+    $blur.css('background-image',$('header:first-of-type').css('background-image'));
+  },
+
+
+  onScroll: function() {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
+
+  
+  requestTick: function() {
+    if( !this.ticking ) {
+      window.requestAnimFrame(this.update.bind(this));
+    }
+    this.ticking = true;
+  },
+
+  update: function() {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking       = false;
     
-   $(function() {
-
-
     
+    var slowScroll = currentScrollY / 2
+      , blurScroll = currentScrollY * 2
+      , opaScroll = 1.4 - currentScrollY / 400;
+   if(currentScrollY > wHeight)
+     $('nav').css('position','fixed');
+   else
+     $('nav').css('position','absolute');
     
+    $content.css({
+      'transform'         : 'translateY(' + slowScroll + 'px)',
+      '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
+      '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
+      'opacity' : opaScroll
+    });
+    
+    $blur.css({
+      'opacity' : blurScroll / wHeight
+    });
+  }
+};
 
-<<<<<<< HEAD
+
 var scroller = new Scroller();  
 scroller.init();
-    
-   $(function() {
-=======
->>>>>>> parent of c9a1a10... updateIndex
-=======
-        <div style="" id="mainContainer">
-                <div style="margin-top:10%" class="">
-                        <h1 class="text-white text-center" style="font-size:50px;">Hi ! Please get more information from here to know me . </h1>
-                    </div>
-        </div>
-        <!--以下是list的畫面，應該放在商品資訊那頁-->
-        <!-- <div id="categoryAndImage" class="container " >
-            <div id="watchsCategory" class="col-3" style="margin-left:-12%;">
-                <ul class="list-group" style="">
-                        <li class="list-group-item list-group-item-dark ">Cras justo odio</li>
-                        <li class="list-group-item list-group-item-dark">Dapibus ac facilisis in</li>
-                        <li class="list-group-item list-group-item-dark">Morbi leo risus</li>
-                        <li class="list-group-item list-group-item-dark">Porta ac consectetur ac</li>
-                        <li class="list-group-item list-group-item-dark">Vestibulum at eros</li>
-                </ul>
-            </div>
-        </div> -->
-
-<script>
-
-    
-   $(function() {
+////////////////////////////
 
 
-    
-    
 
->>>>>>> parent of c9a1a10... updateIndex
-    /*拿資料為主
-    $('#logOutButton').click(function(){
-        $.ajax({
-            method:'GET',
-            url:'logout',
-            data:{},
-            success: function( response ){
-                // window.location.href = "http://127.0.0.1:8000/";
-            },
-            error: function( e ) {
-                console.log(e);
-            }
-        });
-    });
-    */
-    
-    
-    });	
 
 </script>
 
-    </body>
+</body>
 </html>
